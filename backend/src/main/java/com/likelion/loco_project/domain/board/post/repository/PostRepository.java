5ef0e9f62d69981d30de60594503492593b1df34
pickipi <sewd1224@gmail.com>
2025-05-09 13:21:30 +0900

    update: post Repository 및 ë¸ 로컬 브랜치 원격으로 업로드 (#7)

--- backend/src/main/java/com/likelion/loco_project/domain/board/post/repository/PostRepository.java
+++ backend/src/main/java/com/likelion/loco_project/domain/board/post/repository/PostRepository.java
@@ -3,6 +3,8 @@ package com.likelion.loco_project.domain.board.post.repository;
 import com.likelion.loco_project.domain.board.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+import java.util.Optional;
 
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Optional<Post> findById(Long id); // id를 이용하여 게시글 찾기
 }
