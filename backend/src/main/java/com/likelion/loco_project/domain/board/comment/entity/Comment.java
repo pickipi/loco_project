466f5453bc1caed20a531710aeb7e2da8a8965c2
pickipi <sewd1224@gmail.com>
2025-05-14 13:21:52 +0900

    feat: Comment_댓글 엔티티 작성 및 관계 매핑 (#31)

--- backend/src/main/java/com/likelion/loco_project/domain/board/comment/entity/Comment.java
+++ backend/src/main/java/com/likelion/loco_project/domain/board/comment/entity/Comment.java
@@ -1,4 +1,43 @@
 package com.likelion.loco_project.domain.board.comment.entity;
 
-public class Comment {
+import com.likelion.loco_project.domain.board.board.entity.Board;
+import com.likelion.loco_project.domain.user.entity.User;
+import com.likelion.loco_project.global.jpa.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false)
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private boolean isDeleted = false;
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
+
+    public void delete() {
+        this.isDeleted = true;
+    }
 }
