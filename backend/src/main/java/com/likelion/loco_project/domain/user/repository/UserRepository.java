8d54ac18dab4da1d1f8982f2dd22c0a5079d8ffe
pickipi <sewd1224@gmail.com>
2025-05-15 09:32:39 +0900

    fix: BoardService 관련 Repo -> Repository 어노테이션 추가 (Host, User)

--- backend/src/main/java/com/likelion/loco_project/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/likelion/loco_project/domain/user/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.likelion.loco_project.domain.user.repository;
 
 import com.likelion.loco_project.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
