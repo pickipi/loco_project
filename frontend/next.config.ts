dd07eb2a3872bb49ca4afa9b4a331a3f59ad21c2
pickipi <sewd1224@gmail.com>
2025-05-19 09:35:46 +0900

    fix: 프론트 설정 파일 수정 (실행테스트완료)

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["example.com"],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
