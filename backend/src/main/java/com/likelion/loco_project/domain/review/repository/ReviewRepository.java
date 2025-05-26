a79b13e0fd6412939eced993cd66edcef6ca45ec
Jong-min-choi <juktoma94@naver.com>
2025-05-26 11:25:26 +0900

    fix/#101: fix rate/report/review

--- backend/src/main/java/com/likelion/loco_project/domain/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/likelion/loco_project/domain/review/repository/ReviewRepository.java
@@ -2,7 +2,6 @@ package com.likelion.loco_project.domain.review.repository;
 
 import com.likelion.loco_project.domain.review.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
