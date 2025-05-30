44873b834b5d5ebcfe0ceab8577f78cf59b9b710
Jong-min-choi <juktoma94@naver.com>
2025-05-30 14:30:56 +0900

    feature/#131: create notification and space reservation

--- backend/src/main/java/com/likelion/loco_project/domain/space/dto/SpaceReservationResponseDto.java
+++ backend/src/main/java/com/likelion/loco_project/domain/space/dto/SpaceReservationResponseDto.java
@@ -1,15 +1,29 @@
 package com.likelion.loco_project.domain.space.dto;
 
+import com.likelion.loco_project.domain.space.entity.SpaceReservation;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
-public class SpaceReservationRequestDto {
+@Builder
+public class SpaceReservationResponseDto {
+    private Long reservationId;
     private Long guestId;
     private Long spaceId;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
+    private LocalDateTime createdAt;
+
+    public static SpaceReservationResponseDto from(SpaceReservation reservation) {
+        return SpaceReservationResponseDto.builder()
+                .reservationId(reservation.getId())
+                .guestId(reservation.getGuest().getId())
+                .spaceId(reservation.getSpace().getId())
+                .startTime(reservation.getStartTime())
+                .endTime(reservation.getEndTime())
+                .createdAt(reservation.getCreatedAt())
+                .build();
+    }
 }
